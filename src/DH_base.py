c7fe2d20ed3e060946ed4eddf74f1212e51eb9f5
Karlass <gymkrom1@seznam.cz>
2021-10-22 19:22:45 +0200

    optimalizace isprime, zmeny velikosti g

--- src/DH_base.py
+++ src/DH_base.py
@@ -2,22 +2,21 @@ import random, math
 
 bitlen = int(input("bits p = "))
 def isprime(prim):
-    prime = True
     if(prim % 2 == 0):
-        prime = False
+        return False
     else:
         root = int(math.sqrt(prim)+1)
         for i in range(3,root,2):
          if(prim % i == 0):
-            prime = False
-    return prime
+            return False
+    return True
 p = random.getrandbits(bitlen)
 if (p % 2 == 0):
     p += 1
 while(not isprime(p)):
     p += 2
 print("p = "+ str(p))
-g = random.randint(int(math.sqrt(p)), p-1) #toto je problem, ten generator je pekne naprd pocitat
+g = random.randint(int(p**(1/6)), int(p**(1/2))) #toto je problem, ten generator je pekne naprd pocitat
 print("g = "+ str(g))
 sec_a = random.randint(1, p-1) #klice pres random
 sec_b = random.randint(1, p-1) 
@@ -26,6 +25,10 @@ pub_b = g**sec_b % p
 final_a = pub_b ** sec_a % p #výpočet klice pro oba, jestli funguje jsou stejne
 final_b = pub_a ** sec_b % p
 print("tajne klice alice a bob: " + str(final_a) + ", " + str(final_b))
+
+
+
+
 
 
 
